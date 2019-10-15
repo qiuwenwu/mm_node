31cc4e15fde80790e5122bfa996cdf908d482274
e <573242395@qq.com>
2019-10-15 18:09:28 +0800

    版本1.0

--- static/js/mm_sdk.js
+++ static/js/mm_sdk.js
@@ -2133,17 +2133,6 @@ if (typeof($) === "undefined") {
 })();
 
 (function() {
-	$.platform = {
-		isWechat: function() {
-			var ua = window.navigator.userAgent.toLowerCase();
-			if (ua.match(/MicroMessenger/i) == 'micromessenger') {
-				return true; // 是微信端
-			} else {
-				return false;
-			}
-		}
-	};
-
 	$.html = {
 		tag: function(tag, prop, value) {
 			var obj;
@@ -2159,5 +2148,74 @@ if (typeof($) === "undefined") {
 		}
 	};
 	
+	$.db = {
+		/// 设置值
+		/// key: 键
+		/// value: 值
+		set: function(key, value) {
+			window.localStorage.setItem(key, value);
+		},
+		/// 获取值
+		/// key: 键
+		/// value: 值
+		get: function(key) {
+			return window.localStorage.getItem(key);
+		},
+		/// 删除值
+		/// key: 键
+		/// value: 值
+		del: function(key) {
+			window.localStorage.removeItem(key);
+		}
+	};
+	
+	
+	$.browser = {
+		versions: function() {
+			var u = navigator.userAgent,
+				app = navigator.appVersion;
+			return { //移动终端浏览器版本信息
+				trident: u.indexOf('Trident') > -1, //IE内核
+				presto: u.indexOf('Presto') > -1, //opera内核
+				webKit: u.indexOf('AppleWebKit') > -1, //苹果、谷歌内核
+				gecko: u.indexOf('Gecko') > -1 && u.indexOf('KHTML') == -1, //火狐内核
+				mobile: !!u.match(/AppleWebKit.*Mobile.*/), //是否为移动终端
+				ios: !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/), //ios终端
+				android: u.indexOf('Android') > -1 || u.indexOf('Linux') > -1, //android终端或uc浏览器
+				iPhone: u.indexOf('iPhone') > -1, //是否为iPhone或者QQHD浏览器
+				iPad: u.indexOf('iPad') > -1, //是否iPad
+				webApp: u.indexOf('Safari') == -1 //是否web应该程序，没有头部与底部
+			};
+		}(),
+		language: (navigator.browserLanguage || navigator.language).toLowerCase(),
+		get: function() {
+			var v = $.browser.versions;
+			if (v.mobile) { //判断是否是移动设备打开。browser代码在下面
+				var ua = navigator.userAgent.toLowerCase(); //获取判断用的对象
+				if (ua.match(/MicroMessenger/i) == "micromessenger") {
+					//在微信中打开
+					return "wechat";
+				}
+				if (ua.match(/WeiBo/i) == "weibo") {
+					//在新浪微博客户端打开
+					return "weibo";
+				}
+				if (ua.match(/QQ/i) == "qq") {
+					//在QQ空间打开
+					return "qq";
+				}
+				if (v.ios) {
+					//是否在IOS浏览器打开
+					return "ios";
+				}
+				if (v.android) {
+					//是否在安卓浏览器打开
+					return "android";
+				}
+			} else {
+				return "pc";
+				//否则就是PC浏览器打开
+			}
+		}
+	};
 })();
-
