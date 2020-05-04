2a2f5da783371feb50fdfaa08b63100fca5fa5f8
qiuwenwu <573242395@qq.com>
2020-05-04 09:10:48 +0800

    更新

--- app/geomancy/plugin/number/api_geomancy_client/list/index.js
+++ app/geomancy/plugin/number/api_geomancy_client/list/index.js
@@ -27,26 +27,24 @@ function only(arr_name, str_name) {
  */
 async function main(ctx, db) {
 	var {
-		page,
-		size,
 		count,
 		number,
 		url,
 		type,
 		list,
 		score_min,
-		price,
 		price_max,
 		name_only,
 		page_now,
 		page_size,
-		number_has
+		number_has,
+		number_not,
+		number_only,
+		price
 	} = ctx.request.query;
-	url = url || "http://lianghao365.com/api/number?cityID=6&kid=1&&page={page}&pageSize={size}&number={number}";
+	url = url || "http://lianghao365.com/api/number?cityID=6&kid=1&&page={page_now}&pageSize={page_size}&number={number_has}&bubaohan={number_not}&max={price_max}&onlynum={number_only}";
 	count = count || "count";
 	number = number || "number";
-	size = size || "size";
-	page = page || "page";
 	type = type || "jobj";
 	list = list || "list";
 	price = price || "price";
@@ -55,9 +53,11 @@ async function main(ctx, db) {
 	page_size = page_size || 10000;
 	page_now = page_now || 1;
 	number_has = number_has || "";
+	number_only = number_only || "";
 	name_only = name_only || "天医 延年 生气 伏位 六煞 五鬼 祸害 绝命 潜藏 显露";
+	number_not = number_not || "";
 	var http = new $.Http();
-	var u = url.replace(`{${size}}`, page_size).replace(`{${page}}`, page_now).replace(`{${number}}`, number_has);
+	var u = url.replace(`{page_size}`, page_size).replace(`{page_now}`, page_now).replace(`{number_has}`, number_has).replace(`{number_not}`, number_not).replace(`{price_max}`, price_max).replace(`{number_only}`, number_only);
 	// console.log(u);
 	var {
 		body
@@ -82,11 +82,9 @@ async function main(ctx, db) {
 			if (lt) {
 				for (var i = 0; i < lt.length; i++) {
 					var o = lt[i];
-					if (o[price] && o[price] <= price_max) {
-						var ret = energy.run(o.number);
-						if (ret.score >= score_min && only(ret.name, name_only)) {
-							arr.push(Object.assign({}, o, ret));
-						}
+					var ret = energy.run(o[number]);
+					if (ret.score >= score_min && only(ret.name, name_only)) {
+						arr.push(Object.assign({}, o, ret));
 					}
 				}
 			}
@@ -101,6 +99,6 @@ async function main(ctx, db) {
 	});
 };
 
-// http://localhost:5000/api/geomancy/list?list=data.list&score_min=40&price=maiJia&price_max=300&name_only=天医 延年 生气 伏位&page_now=1&page_size=100000&number_has=188_
-
+// http://localhost:5000/api/geomancy/list?list=data.list&score_min=20&price=maiJia&price_max=300&name_only=天医 延年 生气 伏位&page_now=1&page_size=100000&number_has=188_
+// http://localhost:5000/api/geomancy/list?list=data.list&score_min=2&price=maiJia&price_max=1000&name_only=天医 延年 生气 伏位&page_now=1&page_size=100000&number_not=47502
 exports.main = main;
